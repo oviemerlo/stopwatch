d38d3f6d0d5a9266526d264fd5617a125c3e145a
Merlo <user@users-MacBook-Pro.local>
2024-05-25 22:42:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 
-function App() {
+const App = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef();
+
+  const start = () => {
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTime(prev => prev + 1);
+    }, 100);
+  };
+
+  const stop = () => {
+    setIsRunning(false);
+    clearInterval(intervalRef.current);
+  };
+
+  const reset = () => {
+    stop();
+    setTime(0);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    const minute = Math.floor(timeInSeconds / (60 * 100));
+    const seconds = Math.floor(timeInSeconds /100 % 60);
+    const millisecond = timeInSeconds % 10
+    return `${String(minute).padStart(2, '0')} : ${String(seconds).padStart(2, '0')} : ${String(millisecond).padStart(2, '0')}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1 className='display'>{formatTime(time)}</h1>
+      <div className='watch'>
+        {!isRunning ? (
+          <button className='start' onClick={start}>Start</button>
+        ) : (
+          <button className='stop' onClick={stop}>Stop</button>
+        )}
+
+        <button  className='reset' onClick={reset}>Reset</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
